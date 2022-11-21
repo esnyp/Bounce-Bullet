d5d6d00e72933264a18823a59e0c5366039a6365
esnyp <1915345@brunel.ac.uk>
2022-11-21 05:21:11 +0000

    New powerup, updated player scale
    New scale power up and player scale

--- Assets/Scripts/BallSpawnerController.cs
+++ Assets/Scripts/BallSpawnerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BallSpawnerController : MonoBehaviour
 {
     public GameObject ball;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/powerUpScaleDetect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerUpScaleDetect : MonoBehaviour
+{
+    PlayerControl playerControl;
+    [SerializeField] GameObject player;
+  
+
+    private void Awake()
+    {
+        playerControl = player.GetComponent<PlayerControl>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector3 newSize = new Vector3(player.transform.localScale.x* (float)1.2, 
+            player.transform.localScale.y * (float)1.2,
+            player.transform.localScale.z * (float)1.2);
+
+        Vector3 a = player.transform.localScale;
+        player.transform.localScale = newSize;
+    }
+}
